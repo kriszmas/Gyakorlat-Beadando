f73428c63f66d05c1124795b5befb5ef53e6c13a
kriszmas <129957862+kriszmas@users.noreply.github.com>
2023-11-25 00:23:26 +0100

    Add files via upload

---
+++ részállapotok/ZYEEHM-Gyakorlat-Beadando5/Java-Spring-Boot-5/demo/src/main/java/com/example/securityrole/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.securityrole;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
